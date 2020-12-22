f8c1cc1ed52bb208fea1fbead3ff264bd5e9269a
susumu2357 <susumu.0112357@gmail.com>
2020-12-22 22:58:06 +0100

    add more years

--- app/src/pages/page1.py
+++ app/src/pages/page1.py
@@ -15,45 +15,58 @@ def load_pickle(url):
     with gzip.open(gzip_file, 'rb') as f:
         return pickle.load(f)
 
-url = 'https://storage.googleapis.com/test_bucket_20200914/year3_with_location_2017_2019.pkl.gz'
+url = 'https://storage.googleapis.com/test_bucket_20200914/year3_with_location_2015_2019.pkl.gz'
 
 # load
 school_df_dict = load_pickle(url)
+
+@st.cache
+def load_df():
+    df_year = {}
+    commun_year = {}
+
+    for year in ['2015', '2016', '2017', '2018', '2019']:
+        df1 = school_df_dict[year]['Math']
+        df2 = school_df_dict[year]['Swedish']
+
+        df1['achievement_percentage'] = df1['achievement_percentage'].replace('.', '0').replace('..', '0').astype(float)
+        df2['achievement_percentage'] = df2['achievement_percentage'].replace('.', '0').replace('..', '0').astype(float)
+
+        df_year[year] = df1.groupby(['school', 'school_commun', 'type', 'nr_students', 'formatted_address', 'latitude', 'longitude'])['achievement_percentage'].median().reset_index()\
+            .merge(df2.groupby('school')['achievement_percentage'].median().reset_index(), on='school')
+        df_year[year] = df_year[year].rename(columns={'achievement_percentage_x':'math_achievement_percentage', 'achievement_percentage_y':'swedish_achievement_percentage'})
+        df_year[year]['achievement_percentage'] = df_year[year][['math_achievement_percentage', 'swedish_achievement_percentage']].mean(axis=1)
+    
+        commun_year[year] = pd.DataFrame(df_year[year].groupby(['school_commun', 'type'])['achievement_percentage']\
+            .describe().sort_values('50%', ascending=False).to_records())
+        commun_year[year]['school_commun_type'] = [a+'_'+b for a,b in zip(commun_year[year]['school_commun'].values, commun_year[year]['type'].values)]
+        commun_year[year]['rank'] = commun_year[year].index + 1
+
+    time_seires_df = pd.concat(
+        [commun_year['2015'][['school_commun_type', 'rank']], commun_year['2016'][['school_commun_type', 'rank']]],
+        ignore_index=True)
+    for year in ['2017', '2018', '2019']:
+        time_seires_df = pd.concat(
+            [time_seires_df, commun_year[year][['school_commun_type', 'rank']]],
+            ignore_index=True)
+    year_list = []
+    for year in ['2015', '2016', '2017', '2018', '2019']:
+        year_list += [year]*len(commun_year[year])
+    time_seires_df = pd.concat(
+        [time_seires_df, pd.Series(year_list, name='year')],
+        axis=1)
+
+    return df_year, commun_year, time_seires_df
 
 class Page1(Page):
     def __init__(self, state):
         self.state = state
 
     def write(self):
+        df_year, commun_year, time_seires_df = load_df()
+
         st.title('Elementary School Grade 3')
         st.header("Statistics of commun")
-
-        df_year = {}
-        commun_year = {}
-
-        for year in ['2017', '2018', '2019']:
-            df1 = school_df_dict[year]['Math']
-            df2 = school_df_dict[year]['Swedish']
-
-            df_year[year] = df1.groupby(['school', 'school_commun', 'type', 'nr_students', 'formatted_address', 'latitude', 'longitude'])['achievement_percentage'].median().reset_index()\
-                .merge(df2.groupby('school')['achievement_percentage'].median().reset_index(), on='school')
-            df_year[year] = df_year[year].rename(columns={'achievement_percentage_x':'math_achievement_percentage', 'achievement_percentage_y':'swedish_achievement_percentage'})
-            df_year[year]['achievement_percentage'] = df_year[year][['math_achievement_percentage', 'swedish_achievement_percentage']].mean(axis=1)
-        
-            commun_year[year] = pd.DataFrame(df_year[year].groupby(['school_commun', 'type'])['achievement_percentage']\
-                .describe().sort_values('50%', ascending=False).to_records())
-            commun_year[year]['school_commun_type'] = [a+'_'+b for a,b in zip(commun_year[year]['school_commun'].values, commun_year[year]['type'].values)]
-            commun_year[year]['rank'] = commun_year[year].index + 1
-
-        time_seires_df = pd.concat(
-            [commun_year['2017'][['school_commun_type', 'rank']], commun_year['2018'][['school_commun_type', 'rank']]],
-            ignore_index=True)
-        time_seires_df = pd.concat(
-            [time_seires_df, commun_year['2019'][['school_commun_type', 'rank']]],
-            ignore_index=True)
-        time_seires_df = pd.concat(
-            [time_seires_df, pd.Series(['2017']*len(commun_year['2017']) + ['2018']*len(commun_year['2018']) + ['2019']*len(commun_year['2019']), name='year')],
-            axis=1)
         
         # interval selection in the scatter plot
         pts = alt.selection(type="interval", encodings=["x"])
@@ -76,7 +89,7 @@ class Page1(Page):
 
         left = (time_series + points).transform_filter(
             pts
-        ).properties(width=300, height=400)
+        ).properties(width=400, height=400)
 
         # right panel: histogram
         mag = alt.Chart().mark_bar().encode(
@@ -105,7 +118,7 @@ class Page1(Page):
         st.subheader('Achievement percentage in average of Math and Swedish')
         option_year = st.selectbox(
         'Choose year',
-        ['2017', '2018', '2019'])
+        ['2015', '2016', '2017', '2018', '2019'])
 
         bars = alt.Chart(commun_year[option_year]).mark_bar().encode(
             x='50%:Q',
@@ -129,9 +142,9 @@ class Page1(Page):
         st.subheader('Achievement percentage of each school in the selected commun')
         option_commun = st.selectbox(
         'Choose commun',
-        df_year[year]['school_commun'].sort_values().unique())
+        df_year[option_year]['school_commun'].sort_values().unique())
 
-        commun_df = df_year[year][df_year[year]['school_commun']==option_commun]
+        commun_df = df_year[option_year][df_year[option_year]['school_commun']==option_commun]
         commun_df = commun_df.sort_values('achievement_percentage', ascending=False)
         
         st.subheader(f"Statistics of {option_commun}")
@@ -145,12 +158,12 @@ class Page1(Page):
             stroked=True,
             filled=True,
             auto_highlight=True,
-            radius_scale=5,
+            radius_scale=3,
             radius_min_pixels=2,
             radius_max_pixels=25,
             line_width_min_pixels=1,
             get_position=['longitude', 'latitude'],
-            get_radius='nr_students',
+            get_radius='achievement_percentage',
             get_fill_color=[255, 140, 0],
             get_line_color=[0, 0, 0],
         )
@@ -168,11 +181,6 @@ class Page1(Page):
             tooltip={"text": "{school}\n{formatted_address}\nachievement_percentage:{achievement_percentage}"})
 
         st.pydeck_chart(r)
-
-        # st.map(
-        #     commun_df,
-        #     tooltip = ['school', 'formatted_address', 'achievement_percentage']
-        # )
 
         base = alt.Chart(commun_df).encode(y=alt.Y('school', sort=None),)
 
@@ -202,7 +210,8 @@ class Page1(Page):
         )
 
         st.write((bar + math + swedish + text).properties(width=600))
-        st.write(commun_df)
+        st.write(commun_df[['school', 'school_commun', 'type', 'nr_students', 
+        'math_achievement_percentage', 'swedish_achievement_percentage', 'achievement_percentage']])
 
 
 
